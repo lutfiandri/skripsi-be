f0138d8f3c90c45b81a85a27763182e114ed6b8c
lutfiandri <lutfiandri232@gmail.com>
2023-11-20 16:20:32 +0700

    feat(device-state): simplify device state model to interface{}

--- internal/model/domain/device_state_domain.go
+++ internal/model/domain/device_state_domain.go
@@ -5,14 +5,8 @@ import (
 )
 
 type DeviceState struct {
-	Id        string          `bson:"_id"`
-	DeviceId  string          `bson:"device_id"`
-	State     DeviceStateData `bson:"state"`
-	CreatedAt time.Time       `bson:"created_at"`
-	UpdatedAt time.Time       `bson:"updated_at"`
-}
-
-type DeviceStateData struct {
-	Online bool `bson:"online"`
-	On     bool `bson:"on"`
+	Id        string      `bson:"_id"`
+	DeviceId  string      `bson:"device_id"`
+	State     interface{} `bson:"state"`
+	CreatedAt time.Time   `bson:"created_at"`
 }
