f52f4aad24a94e5551c9de94c96e88ea27f2f227
lutfiandri <lutfiandri232@gmail.com>
2024-05-27 13:11:32 +0700

    feat(roles-and-permissions): create database seeder for admin, roles, and permissions

--- cmd/seeder/main.go
+++ cmd/seeder/main.go
@@ -1,36 +1,34 @@
 package main
 
 import (
-	"context"
-
+	"skripsi-be/cmd/seeder/seed"
 	"skripsi-be/internal/config"
-	"skripsi-be/internal/domain"
 	"skripsi-be/internal/infrastructure"
-
-	"github.com/google/uuid"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func main() {
 	mongo := infrastructure.NewMongoDatabase(config.MongoUri, config.MongoDbName)
-	seedOauthScopes(mongo)
+	// seedOauthScopes(mongo)
+
+	seed.SeedUsers(mongo)
+	seed.SeedRolesAndPermissions(mongo)
 }
 
-func seedOauthScopes(mongo *mongo.Database) {
-	data := []interface{}{
-		domain.OAuthScope{
-			Id:          uuid.New(),
-			Section:     "Device State",
-			Code:        "read:device_state",
-			Description: "Read device states",
-		},
-		domain.OAuthScope{
-			Id:          uuid.New(),
-			Section:     "Device State",
-			Code:        "write:device_state",
-			Description: "Write device states",
-		},
-	}
+// func seedOauthScopes(mongo *mongo.Database) {
+// 	data := []interface{}{
+// 		domain.OAuthScope{
+// 			Id:          uuid.New(),
+// 			Section:     "Device State",
+// 			Code:        "read:device_state",
+// 			Description: "Read device states",
+// 		},
+// 		domain.OAuthScope{
+// 			Id:          uuid.New(),
+// 			Section:     "Device State",
+// 			Code:        "write:device_state",
+// 			Description: "Write device states",
+// 		},
+// 	}
 
-	mongo.Collection("oauth_scopes").InsertMany(context.Background(), data)
-}
+// 	mongo.Collection("oauth_scopes").InsertMany(context.Background(), data)
+// }
