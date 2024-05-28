ac10c415b8b9c79e5d486d91eb8134b7e30d306a
lutfiandri <lutfiandri232@gmail.com>
2024-05-28 11:40:06 +0700

    feat(roles-and-permissions): add scopes to oauth clients

--- internal/app/oauth_client/rest.go
+++ internal/app/oauth_client/rest.go
@@ -7,13 +7,15 @@ type OAuthClientResponse struct {
 	Secret       string    `json:"secret"`
 	Name         string    `json:"name"`
 	RedirectUris []string  `json:"redirect_uris"`
+	ScopeIds     []string  `json:"scope_ids"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 type OAuthClientPublicResponse struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
+	Id       string   `json:"id"`
+	Name     string   `json:"name"`
+	ScopeIds []string `json:"scope_ids"`
 }
 
 type GetOAuthClientRequest struct {
@@ -23,12 +25,14 @@ type GetOAuthClientRequest struct {
 type CreateOAuthClientRequest struct {
 	Name         string   `json:"name" validate:"required"`
 	RedirectUris []string `json:"redirect_uris" validate:"min=0"`
+	ScopeIds     []string `json:"scope_ids"`
 }
 
 type UpdateOAuthClientRequest struct {
 	Id           string   `params:"id" validate:"required"`
 	Name         string   `json:"name" validate:"required"`
 	RedirectUris []string `json:"redirect_uris" validate:"min=0"`
+	ScopeIds     []string `json:"scope_ids"`
 }
 
 type DeleteOAuthClientRequest struct {
